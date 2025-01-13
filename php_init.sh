82fbd5847d3acbefae479cfab9c2f63351a28db4
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-01-13 08:15:09 +0700

    use file.sh

---
+++ php_init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /app/app_laravel/storage /app/app_laravel/bootstrap/cache /app/app_laravel/database
+chown -R www-data:www-data /app/app_laravel/storage /app/app_laravel/bootstrap/cache /app/app_laravel/database
+chmod -R 777 /app/app_laravel/storage /app/app_laravel/bootstrap/cache /app/app_laravel/database
+php-fpm
