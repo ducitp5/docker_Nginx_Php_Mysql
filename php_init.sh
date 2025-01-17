4fc82430d51b61118df5625c79ac80158c45113c
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-01-18 05:42:44 +0700

    fix  /php_init.sh: line 5: $'php-fpm\r': command not found

--- php_init.sh
+++ php_init.sh
@@ -2,4 +2,5 @@
 mkdir -p /app/app_laravel/storage /app/app_laravel/bootstrap/cache /app/app_laravel/database
 chown -R www-data:www-data /app/app_laravel/storage /app/app_laravel/bootstrap/cache /app/app_laravel/database
 chmod -R 777 /app/app_laravel/storage /app/app_laravel/bootstrap/cache /app/app_laravel/database
+
 php-fpm
